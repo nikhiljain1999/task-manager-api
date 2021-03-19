6c1ec2e23d56f1db6175f2e9ad3460a4054eb8bb
Nikhil Jain <nikhiljain.lanet@gmail.com>
2021-03-19 11:57:00 +0530

    test

--- src/index.js
+++ src/index.js
@@ -51,6 +51,9 @@ app.use(express.json())
 
 app.use(userRouter)
 app.use(taskRouter)
+app.get("/",(req,res)=>{
+    res.send("helloooo");
+})
 
 app.listen(port, () => {
     console.log("Server is up ", port)
